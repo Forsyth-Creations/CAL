9a5f275ca262afbf10ab3de399bdf492ad38cdf3
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2018-09-01 19:48:14 -0400

    New build script

--- build.sh
+++ build.sh
@@ -8,17 +8,18 @@ read answer
 if echo "$answer" | grep -iq "^y" ;then
     echo "Good choice! The install will being shortly"
     sleep 3
+	cd /home/pi/
 	sudo apt-get update && sudo apt-get upgrade
-	cd ~
 	
 	#BLYNK Install
 	echo "Installing Blynk"
 	echo -ne '                          (00%)\r'
 	git clone https://github.com/blynkkk/blynk-library.git
+	cd /home/pi/blynk-library/
 	git checkout dab48f7c89f3eb7d8a6c09a0b4137813644b6817
 	sleep 1
 	echo -ne '#############              (50%)\r'
-	cd blynk-library/linux
+	cd /home/pi/blynk-library/linux
 	make clean all target=raspberry
 	echo "type this to run blynk: sudo ./blynk --token=YourAuthToken"
 	sleep 3
@@ -28,13 +29,12 @@ if echo "$answer" | grep -iq "^y" ;then
 	sleep 1
 	
 	#Lightshowpi Installing
-	cd ~
+	cd /home/pi/
 	echo -ne '##                          (12%)\r'
 	echo "Installing LightshowPi"
 	# Install git (if you don't already have it)
 	sudo apt-get install git-core
 	# Clone the repository to /home/pi/lightshowpi
-	cd ~
 	sleep 1
 	git clone https://togiles@bitbucket.org/togiles/lightshowpi.git
 	echo -ne '#############              (50%)\r'

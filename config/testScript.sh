a56c81ee66578875d86d18c787a479b607a377cb
Robert Forsyth <rhfk3@schls.albco>
2018-11-18 20:08:37 -0500

    New Test

--- config/testScript.sh
+++ config/testScript.sh
@@ -4,10 +4,10 @@
 
 #cat authCode.txt
 
-#declare content =`sed -n '2p' < authCode.txt`
+content = sed -n '2 p' < authCode.txt
 
 #sed -n '1,2p;3q' authCode.txt
-sed -n '2 p' < authCode.txt
 
+echo content
 
 sleep 4

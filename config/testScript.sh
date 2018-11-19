a508fbabbd8041fb338931ea5998a5543daacb1f
Robert Forsyth <rhfk3@schls.albco>
2018-11-18 20:32:50 -0500

    new test script

--- config/testScript.sh
+++ config/testScript.sh
@@ -1,10 +1,2 @@
-#content = `sed -n "$2 p" authCode.txt`
-#content=$(sed -n "$2 p" authCode.txt)
-#echo $content
-
-#cat authCode.txt
-
-value=$(<settings.txt)
+value=$(cat -n settings.txt)
 echo "$value"
-
-sleep 4

a9313d41af0f427851cd0c1a992c4419f357c80e
Robert Forsyth <rhfk3@schls.albco>
2018-11-21 20:52:14 -0500

    New Stuff

--- examples/CAL/allCalPinsOn.sh
+++ examples/CAL/allCalPinsOn.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-
-pinsUsed=(0 1 2 3 4 5 6 21) # Pins Used for the relay board, based on wiringPi specs
-
-for item in ${pinsUsed[*]} # For Every item in the pinsUsed list
+pinsUsed = (0 1 2 3 4 5 6 21) 
+for item in ${pinsUsed[*]} 
 do
-        gpio mode $item out # Set gpio pinMode to OUT
-        gpio write $item 0 # Write high to the designated pin
-        #sleep 0.1 #Delay 
-		#printf "   %s\n" $item # Lists item actived
+        gpio mode $item out 
+        gpio write $item 0 
+        #sleep 0.1 Delay 
+		#printf "   %s\n" $item Lists item actived
 done
-
 exit

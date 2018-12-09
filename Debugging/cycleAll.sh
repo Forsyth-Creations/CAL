6c1eb3997632189bef9a475334b9030109b14546
Robert Forsyth <rhfk3@schls.albco>
2018-12-09 11:13:32 -0500

    cleaning up branch

--- Debugging/cycleAll.sh
+++ Debugging/cycleAll.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-#All pins used are based on the WiringPi configuration. 
-#Please go online to determine corresponding 
-#hardware pins to software 
+pinsUsed=(0 11 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26) #Pins Used for the relay board, based on wiringPi specs
 
-pinsUsed=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25) 
-
-for item in ${pinsUsed[*]} 
+for item in ${pinsUsed[*]} #For Every item in the pinsUsed list
 do
-        gpio mode $item out 
-        gpio write $item 0 
-        sleep 4 #Delay 
-        printf "   %s\n" $item #Lists items
-        gpio write $item 1
-        sleep 1
+    gpio mode $item out #Set gpio pinMode to OUT
+    gpio write $item 0 #Turns lights on
+	printf "   %s\n" $item #Lists item actived
+	sleep 2 #Delay
+	gpio write $item 1 #Turns lights off
+	sleep 1
 done
+
 exit

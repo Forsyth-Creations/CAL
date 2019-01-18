47a8c36e0d77104c855bcddafb68a1ba447aefc0
rhfk3 <rhfk3@schls.albco>
2019-01-17 23:28:06 -0500

    Nwe Update Code

--- coreCode/testVariableSet.sh
+++
@@ -1,6 +0,0 @@
-#Test Test
-
-#authCode=$(`sed 's/[^0-9]//g' /home/pi/authCode.txt` | sed 's/ /,/g'``)
-authCode=$(sed '' /home/pi/authCode.txt| xargs | sed 's/ /,/g')
-echo $authCode
-
---
+++ coreCode/update.sh
@@ -0,0 +1,9 @@
+#git status
+
+
+cd /home/pi/CAL/
+git fetch --all
+git reset --hard origin/master
+cd Music/	
+git pull 
+echo "Update Complete"

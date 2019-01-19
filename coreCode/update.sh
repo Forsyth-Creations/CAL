933e11977eb1922e2ef68294c751d4ff27aa3476
rhfk3 <rhfk3@schls.albco>
2019-01-18 22:07:57 -0500

    New update code

--- coreCode/update.sh
+++ coreCode/update.sh
@@ -1,7 +1,10 @@
 #git status
 
+#UPDATE V1
+#cd /home/pi/CAL/
+#git fetch --all
+#git reset --hard origin/master 
+#echo "Update Complete"
 
-cd /home/pi/CAL/
-git fetch --all
-git reset --hard origin/master 
-echo "Update Complete"
+#UPDATE V2
+git pull origin/master

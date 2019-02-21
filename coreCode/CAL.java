0e6beed2bccbb9bc80c69f3ae3aa47aaec520022
rhfk3 <rhfk3@schls.albco>
2019-02-21 13:29:55 -0500

    New Java code

--- coreCode/CAL.java
+++ coreCode/CAL.java
@@ -1,10 +1,10 @@
 public class CAL {
     public static void main(String[] args) {
 		System.out.println("     _______   ___   ___               ");
-		System.out.println("    /  ____/  / _ \  \  \              ");
-		System.out.println("   /  /      / /_\ \  \  \             ");
-		System.out.println("  /  /___   /  / \  \  \  \___         ");
-		System.out.println(" /______/  /__/   \__\  \_____\   V.4  ");
+		System.out.println("    /  ____/  / _ \\  \\  \\              ");
+		System.out.println("   /  /      / /_\\ \\  \\  \\             ");
+		System.out.println("  /  /___   /  / \\  \\  \\  \\___         ");
+		System.out.println(" /______/  /__/   \\__\\  \\_____\\   V.4  ");
     }
 
 }

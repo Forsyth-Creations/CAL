adbec830d0bdf8c68f9e86485ec5c22263cedf9c
rhfk3 <rhfk3@schls.albco>
2019-02-21 14:03:25 -0500

    New init

--- coreCode/CALinitialize.sh
+++ coreCode/CALinitialize.sh
@@ -9,7 +9,6 @@ blynkActivationState=${blynkActivationState:9}
 lightshowActivationState=${lightshowActivationState:13}
 
 #Pull Latest code from github to run
-
 #cd /home/pi/CAL/
 #git fetch --all
 #git reset --hard origin/master
@@ -26,81 +25,33 @@ echo " /______/  /__/   \__\  \_____\   V.4  "
 echo " "
 echo " "
 
-
-#Getting Rid of Spaces
+#----------Getting Rid of Spaces------------------
 cd /home/pi/CAL/Music
 echo " "
 echo "Getting rid of spaces"
-#sleep 1
 find . -depth -name '* *' \
 | while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
-#sleep 1
-
 cd
-echo " "
-#sleep 2
-#echo "Mounting USB..."
+#----------Mount USB Device (If Needed) -----------
+#echo "Mounting USB...
 #sudo mount /dev/sda1 usbDevice
 #echo " "
-
-#sleep 1
+#--------Make Playlist from Songs------------------
 echo "Making playlist from GitHub songs"
 cd /home/pi/lightshowpi/tools
 echo /home/pi/CAL/Music | python /home/pi/lightshowpi/tools/playlist_generator.py &> /dev/null &
 echo " "
 echo "Playlist was created"
 echo " "
-
-#sleep 1
-
+#--------Kill All Blynk Scripts--------------------
 sudo killall blynk
 sleep 2
 #echo "Running Blynk"
-
+#------------Activate Blynk-----------------------
 if [ $blynkActivationState = "true" ] || [ $blynkActivationState = "True" ]
 then
 	echo "Running Blynk for this unit. Please change Auth code if CAL installed on new unit"
 	cd /home/pi/blynk-library/linux/ && sudo ./blynk --token=$authCode &> /dev/null &
 fi
-#sleep 8
-
-#Getting Example Scripts Ready
-#echo "Getting Example Scripts Ready"
-
-examplePath='/home/pi/CAL/examples/'
-: '
-cd /home/pi/CAL/examples/CAL
-
-#chmod +x allOn.sh
-#chmod +x allOff.sh
-
-cd /home/pi/CAL/examples/CAL_JUNIOR
-
-#chmod +x allOn.sh
-#chmod +x allOff.sh
-#chmod +x cycleThroughOutlets.sh
-
-cd /home/pi/CAL/examples/CAL_MINI
-
-#chmod +x allOn.sh
-#chmod +x allOff.sh
-
-cd /home/pi/CAL/lightshowCommands
-
-#chmod +x beginShow.sh
-#chmod +x stopShow.sh
-
-
-cd 
-
-cd /home/pi/CAL/Debugging/
-#chmod +x cycleAll.sh
-
-echo " "
-echo "All startup procedures have been run, and youre ready to go! Type anything to exit"
-
-cd
-' 
-
 exit
 

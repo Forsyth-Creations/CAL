c4917cc6d0fac9666199e8aa963dfa8b70bc4203
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-01 14:06:55 -0500

    new on code

--- examples/NEW_allOn.sh
+++ examples/NEW_allOn.sh
@@ -4,15 +4,34 @@ cd /home/pi/CAL/config
 pinsUsed=$(sed -n '14p' < overrides.cfg)
 pinsUsed=${pinsUsed:9}
 
-pinsHigh=$(sed -n '14p' < overrides.cfg)
-pinsHigh=${pinsHigh:15}
+pinsState=$(sed -n '10p' < overrides.cfg)
+pinsState=${pinsState:15}
+
+#helps determine if I need to turn the lights
+#on or off
+
+for arg in "$@"
+do
+    if [ "$arg" == "on" ] || [ "$arg" == "ON" ]
+    then
+        echo "Lights Turned On"
+    else
+	echo "Lights Turned Off"
+       if [ pinsState= == 1 ]
+       then
+           pinsState= 0
+       else
+	   pinsState= 1
+    fi
+done
+
 
 #pinsUsed=(4 1 16 15) #Pins Used for the relay board, based on wiringPi specs
 
 for item in ${pinsUsed[*]} #For Every item in the pinsUsed list
 do
         gpio mode $item out #Set gpio pinMode to OUT
-        gpio write $item pinsHigh #Turn the designated pin on
+        gpio write $item pinsState #Turn the designated pin on
         sleep 0.5 #Delay (If you want that)
 	printf "   %s\n" $item #(Lists item actived)
 done

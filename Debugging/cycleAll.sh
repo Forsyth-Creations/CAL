ff2dc230621a23ad07e4b873c5759cc14f44c0ea
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-01 13:19:31 -0500

    New Debug Code

--- Debugging/cycleAll.sh
+++ Debugging/cycleAll.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pinsUsed=(0 11 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26) #Pins Used for the relay board, based on wiringPi specs
+pinsUsed=(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26) #Pins Used for the relay board, based on wiringPi specs
 
 for item in ${pinsUsed[*]} #For Every item in the pinsUsed list
 do

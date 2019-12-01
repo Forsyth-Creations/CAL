8d7f1a92c1d9fe4a1c5d2c76056ed0440098177c
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-01 13:56:00 -0500

    new on code

--- examples/NEW_allOn.sh
+++ examples/NEW_allOn.sh
@@ -4,12 +4,15 @@ cd /home/pi/CAL/config
 pinsUsed=$(sed -n '14p' < overrides.cfg)
 pinsUsed=${pinsUsed:9}
 
+pinsHigh=$(sed -n '14p' < overrides.cfg)
+pinsHigh=${pinsHigh:15}
+
 #pinsUsed=(4 1 16 15) #Pins Used for the relay board, based on wiringPi specs
 
 for item in ${pinsUsed[*]} #For Every item in the pinsUsed list
 do
         gpio mode $item out #Set gpio pinMode to OUT
-        gpio write $item 0 #Write low to the designated pin
+        gpio write $item pinsHigh #Turn the designated pin on
         sleep 0.5 #Delay (If you want that)
 	printf "   %s\n" $item #(Lists item actived)
 done

ab69411264ec96c43aa081a511af40855318bc2d
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-01 13:37:57 -0500

    New allOn script based on config

---
+++ Server Java Code 2019-2020/Server.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Compilation:  javac Server.java
+ *  Execution:    java Server
+ *
+ *  This program will eventually create a server
+ *  for the CAL lighting and smart outlet system
+ *
+ ******************************************************************************/
+
+public class Server
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}

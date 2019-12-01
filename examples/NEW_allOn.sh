c0d5334c2464f03efc79cae876a571309f2cf302
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-01 13:48:14 -0500

    New all on code

--- examples/NEW_allOn.sh
+++ examples/NEW_allOn.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-pinsUsed=(4 1 16 15) #Pins Used for the relay board, based on wiringPi specs
+pinsUsed=$(sed -n '14p' < overrides.cfg)
+pinsUsed=${pinsUsed:9}
+
+#pinsUsed=(4 1 16 15) #Pins Used for the relay board, based on wiringPi specs
 
 for item in ${pinsUsed[*]} #For Every item in the pinsUsed list
 do
         gpio mode $item out #Set gpio pinMode to OUT
         gpio write $item 0 #Write low to the designated pin
-        #sleep 0.1 #Delay (If you want that)
-	#printf "   %s\n" $item #(Lists item actived)
+        sleep 0.5 #Delay (If you want that)
+	printf "   %s\n" $item #(Lists item actived)
 done
 
 exit

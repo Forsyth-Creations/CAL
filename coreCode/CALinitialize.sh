9d2137f77ad88cd7504309cbeddd43732010ed1c
HullabalooRobotics <33364183+HullabalooRobotics@users.noreply.github.com>
2019-12-22 14:54:50 -0500

    Allow functinoality to deactivate auto update

--- coreCode/CALinitialize.sh
+++ coreCode/CALinitialize.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# --- Testing to see if the settings override file exists ---
 cd /home/pi/CAL/config/
 FILE=overrides.cfg
 if test -f "$FILE"; 
@@ -8,7 +9,9 @@ then
 else
     FILE=settings.txt
 fi
+#-----------------------------------------------------------
 
+#---------- Making all relavent variables ------------------
 cd /home/pi/CAL/config
 authCode=$(sed -n '4p' < $FILE)
 authCode=${authCode:9}
@@ -16,21 +19,13 @@ blynkActivationState=$(sed -n '2p' < $FILE)
 lightshowActivationState=$(sed -n '3p' < $FILE)
 blynkActivationState=${blynkActivationState:9}
 lightshowActivationState=${lightshowActivationState:13}
-
 activateAutoUpdate=$(sed -n '13p' < $FILE)
 activateAutoUpdate=${activateAutoUpdate:11}
+#-------------------------------------------------------------
 
-#Pull Latest code from github to run
-cd /home/pi/CAL/
-git fetch --all >/dev/null 2>&1
-git reset --hard origin/master >/dev/null 2>&1
-git pull origin >/dev/null 2>&1
-#cd Music/	
-#git pull 
-
+#---------------------- Logo ---------------------------------
 clear
 cd
-echo " "
 echo "     _______   ___   ___               "
 echo "    /  ____/  / _ \  \  \              "
 echo "   /  /      / /_\ \  \  \             "
@@ -38,8 +33,9 @@ echo "  /  /___   /  / \  \  \  \___         "
 echo " /______/  /__/   \__\  \_____\   V.6  "
 echo " "
 #echo " "
+#------------------------------------------------------------
 
-#----------Getting Rid of Spaces------------------
+#------------------Getting Rid of Spaces---------------------
 cd /home/pi/CAL/Music
 #echo " "
 #echo "Getting rid of spaces"
@@ -48,23 +44,28 @@ sleep 1
 find . -depth -name '* *' \
 | while IFS= read -r f ; do mv -i "$f" "$(dirname "$f")/$(basename "$f"|tr ' ' _)" ; done
 cd
+#-----------------------------------------------------------
+
 #----------Mount USB Device (If Needed) -----------
 #echo "Mounting USB...
 #sudo mount /dev/sda1 usbDevice
 #echo " "
+#--------------------------------------------------
+
 #--------Make Playlist from Songs------------------
-#echo "Making playlist from GitHub songs"
 echo -ne '####               (20%) Making playlist from GitHub songs\r'
 sleep 1
 cd /home/pi/lightshowpi/tools
 echo /home/pi/CAL/Music | python /home/pi/lightshowpi/tools/playlist_generator.py &> /dev/null &
-#echo "Playlist was created"
 echo -ne '########           (40%) Playlist was created             \r'
 sleep 1
+#-----------------------------------------------------------
+
 #--------Kill All Blynk Scripts--------------------
 sudo killall blynk
 sleep 1
-#echo "Running Blynk"
+#------------------------------------------------
+
 #-------    Active Auto-Update  ------------------
 #printf "Auto update is: %s\n" $activateAutoUpdate
 if [ $activateAutoUpdate = "on" ] || [ $activateAutoUpdate = "On" ]
@@ -73,7 +74,13 @@ then
 	cd /home/pi/CAL/coreCode/
 	python autoUpdate.py >/dev/null 2>&1 &
 	sleep 1
+else
+	echo -ne '############    (50%) Deactivating Auto Update           \r'
+	cd /home/pi/CAL/coreCode/
+	pkill -f autoUpdate.py >/dev/null 2>&1 &
+	sleep 1
 fi
+#----------------------------------------------------
 
 #------------Activate Blynk-----------------------
 if [ $blynkActivationState = "true" ] || [ $blynkActivationState = "True" ]
@@ -86,4 +93,5 @@ fi
 echo -ne '################(100%) Complete                               \r\n'
 echo "You're all ready to go!"
 exit
+#-------------------------------------------------------------
 
